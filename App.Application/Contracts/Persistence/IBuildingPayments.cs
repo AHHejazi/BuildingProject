c8ece45953d1aceadbd1451999eccdb61c7cb7bb
HejaziO <43334841+HejaziO@users.noreply.github.com>
2021-02-16 14:48:20 -0500

    Ola-v1

---
+++ App.Application/Contracts/Persistence/IBuildingPayments.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Application.Contracts.Persistence
+{
+    interface IBuildingPayments
+    {
+    }
+}
---
+++ App.Domain/Entities/Shareholders.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Domain.Entities
+{
+   public class Shareholders
+    {
+
+
+    }
+}
