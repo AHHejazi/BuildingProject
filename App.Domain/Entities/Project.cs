4dfb48340278304ebfc4dabcaf4227ac8291f48a
HejaziO <43334841+HejaziO@users.noreply.github.com>
2021-02-17 17:06:09 -0500

    Ola V2

--- App.Domain/Entities/Project.cs
+++ App.Domain/Entities/Project.cs
@@ -5,24 +5,43 @@ using System.Text;
 
 namespace App.Domain.Entities
 {
-    public class Project: AuditableEntity
+    public class Project : AuditableEntity
     {
-     
-            public System.Guid Id { get; set; }
-            public string NameAr { get; set; }
-            public string NameEn { get; set; }
-            public string Number { get; set; }
-            public string ContractorName { get; set; }
-            public string ConsultingOfficeName { get; set; }
-            public string CityName { get; set; }
-            public string QuarterName { get; set; }
-            public string PropertyFullAddress { get; set; }
-            public string PropertyNumber { get; set; }
-            public string PropertyLatitude { get; set; }
-            public string PropertyLongitude { get; set; }
-            public bool IsActive { get; set; }
-            public string SerialNumber { get; set; }
-            
-        }
+
+        public System.Guid Id { get; set; }
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public string Number { get; set; }
+        public string ContractorName { get; set; }
+        public string ConsultingOfficeName { get; set; }
+        public string CityName { get; set; }
+        public string QuarterName { get; set; }
+        public string PropertyFullAddress { get; set; }
+        public string PropertyNumber { get; set; }
+        public string PropertyLatitude { get; set; }
+        public string PropertyLongitude { get; set; }
+        public bool IsActive { get; set; }
+        public string SerialNumber { get; set; }
+        ////
+        public int ProjectType { get; set; }
+        public int TotalArea { get; set; }
+        public int InstrumentNumber { get; set; }
+        public int BuildingLicenseNumber { get; set; }
+        public int FloorsNumber { get; set; }
+        public string BuildingTechnique { get; set; }
+        public int EstimatedCost { get; set; }
+        ////
+        public string InstrumentAttachment { get; set; }
+        public string BuildingLicenseAttachment { get; set; }
+        public string ConstructionDiagrams { get; set; }
+        public string MechanicalDiagrams { get; set; }
+        public string ArchitecturalDiagrams { get; set; }
+        public string ElictricalDiagrams { get; set; }
+        public string SoilReport { get; set; }
+        public string SurveyReport { get; set; }
+        public int StocksNumber { get; set; }
+        public int Cost { get; set; }
+
     }
+}
 
