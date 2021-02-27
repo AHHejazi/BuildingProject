c819ac7a181ae80ef273e216b044388e8ed9c14b
HejaziO <43334841+HejaziO@users.noreply.github.com>
2021-02-27 17:08:19 -0500

    27-2 Ola

---
+++ Building.Web/Components/Projects/ProjectDetails.razor.cs
@@ -0,0 +1,28 @@
+﻿using Application.App.Services.Projects;
+using Domain.App.Entities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Building.Web.Components.Projects
+{
+    public partial class ProjectDetails : ComponentBase
+    {
+        [Inject]
+        public IProjectService _projectService { get; set; }
+        public IReadOnlyList<Project> projectList;
+
+
+        protected override void OnInitialized()
+        {
+            projectList = new List<Project>();
+            base.OnInitialized();
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            projectList = await _projectService.ProjectListQuery();
+        }
+    }
+}
